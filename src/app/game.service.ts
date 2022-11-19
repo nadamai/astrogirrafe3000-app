588c0f99129011d34e6b6bbb2e458650a55f0583
Daniel Stasiak <dan@digitibox.pl>
2022-11-19 22:24:40 +0100

    Setting player

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { PlayerService } from './player.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,15 @@ import { Injectable } from '@angular/core';
 export class GameService {
   public stage: string = 'title';
 
+  constructor(
+    public player: PlayerService
+  ) { }
+
   play() {
     this.stage = 'gameplay';
+
+    this.player.x = 50 - (15 / 2);
+    this.player.y = 110;
+    this.player.rotation = 180;
   }
 }
