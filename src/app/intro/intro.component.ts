7fd6764b60a61adb5ec99c864142f456aa5d5de4
Daniel Stasiak <dan@digitibox.pl>
2022-11-19 12:40:53 +0100

    Sky done

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
+  @HostBinding('class.start') start: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    setTimeout(() => {
+      this.start = true;
+    }, 1)
 
+  }
 }
